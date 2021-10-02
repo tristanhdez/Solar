4b3a564f3f476e60daf650272a5aaf3ba90adf56
Tristan <christian.hflores@alumnos.udg.mx>
2021-10-01 21:39:06 -0500

    Library in progress

---
+++ src/responses.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var knowledge = {
+    "hola":"¡Hola! Soy Solar, un gusto conocerte.",
+    "comoestas":"Estoy contento de estar contigo",
+    "cualeselreglamentodelaescuela": "En un momento te paso el reglamento",
+    "quienesmitutor": "Tu tutor es:",
+};
+
+
+var rightsAndObligations = {
+
+};
+
+var documentationAndLanguages = {
+
+};
+
+var entertainment = {
+
+};
+
+
+var scholarshipAndSupport = {
+
+};
+
+var others = {
+
+};
+
+var userMessages = {
+    "hola":"¡Hola! Soy Solar, un gusto conocerte.",
+    "comoestas":"Estoy contento de estar contigo",
+    "quehaces":"...",
+    "chiste":"...",
+    "":""
+};
+
